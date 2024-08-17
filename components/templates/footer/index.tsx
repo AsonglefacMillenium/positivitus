d4a3db967d1f2bb5ab13bd7cf383365775f50626
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2024-08-17 11:57:15 +0300

    add footer and contact sections

--- components/templates/footer/index.tsx
+++ components/templates/footer/index.tsx
@@ -29,6 +29,32 @@ const FooterSection = () => {
             <AiFillTwitterCircle />
           </div>
         </div>
+
+        <div className={styles.footer_info}>
+          <div className={styles.info_left}>
+            <div className={styles.info_head}>
+              <h4>Contact Us:</h4>
+            </div>
+
+            <div className={styles.info_content}>
+              <p>Email: info@positivus.com</p>
+              <p>Phone: 555-567-8901</p>
+              <p>Address: 1234 Main St Moonstone City, Stardust State 12345</p>
+            </div>
+          </div>
+
+          <div className={styles.info_right}>
+            <input type="text" placeholder="Email" />
+            <button type="submit">Subscribe to news</button>
+          </div>
+        </div>
+
+        <div className={styles.divider}></div>
+
+        <div className={styles.copyright}>
+          <p>© {new Date().getFullYear()} Positivus. All Rights Reserved.</p>
+          <Link href={""}>Privacy Policy</Link>
+        </div>
       </div>
     </footer>
   );
